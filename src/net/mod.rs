996e3b6e66786d985e3a4ae6e10bf0ce95d3fa5e
Richard Leek <richard@richardleek.com>
2020-12-03 11:09:47 -0500

    removed packet struct added packet_id_hash fn

--- src/net/mod.rs
+++ src/net/mod.rs
@@ -1,8 +1,36 @@
+use crate::data::EOInt;
+
 mod action;
 pub use action::Action;
 
 mod family;
 pub use family::Family;
 
-mod packet;
-pub use packet::Packet;
+/// flattens a packet [Family] and [Action] into a single [EOInt]
+///
+/// This is useful for identifying pairs of packet [Family]s, and [Action]s.
+/// It is assumed there are a max of 255 actions per packet family.
+///
+/// # Example
+/// ```
+/// use eo::net::*;
+///
+/// assert_eq!(packet_id_hash(Family::Connection, Action::Request), 257)
+/// ```
+pub fn packet_id_hash(family: Family, action: Action) -> EOInt {
+    ((family as EOInt) << 8) | action as EOInt
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn packet_id_hash() {
+        assert_eq!(super::packet_id_hash(Family::Init, Action::Init), 65535);
+        assert_eq!(
+            super::packet_id_hash(Family::Connection, Action::Request),
+            257
+        );
+        assert_eq!(super::packet_id_hash(Family::Welcome, Action::Reply), 1283);
+    }
+}
