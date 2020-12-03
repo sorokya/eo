2ba734b5101ef5acda05c32e93fb3dc8cef0db2e
Richard Leek <richard@richardleek.com>
2020-12-02 20:39:43 -0500

    Added some packet structes, tests, and docs

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,11 @@
 //! This crate will contain most of the data structures and methods needed
 //! to create any kind of program for [Endless Online](https://game.eoserv.net/).
 
+#[macro_use]
+extern crate enum_primitive_derive;
+extern crate num_traits;
+
 /// Contains EO primitive types, stream reader, and stream builder
 pub mod data;
+/// Contains Network related types for EO
+pub mod net;
---
+++ src/net/mod.rs
@@ -0,0 +1,8 @@
+mod action;
+pub use action::Action;
+
+mod family;
+pub use family::Family;
+
+mod packet;
+pub use packet::Packet;
