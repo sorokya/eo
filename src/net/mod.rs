614b308309c50cb9f601f087c2e74c1b428ebc1f
Richard Leek <richard@richardleek.com>
2020-12-04 12:26:16 -0500

    added packet processing

--- src/net/mod.rs
+++ src/net/mod.rs
@@ -1,26 +1,7 @@
-use crate::data::EOInt;
-
 mod action;
 pub use action::Action;
-
 mod family;
 pub use family::Family;
-
-/// flattens a packet [Family] and [Action] into a single [EOInt]
-///
-/// This is useful for identifying pairs of packet [Family]s, and [Action]s.
-/// It is assumed there are a max of 255 actions per packet family.
-///
-/// # Example
-/// ```
-/// use eo::net::*;
-///
-/// assert_eq!(packet_id_hash(Family::Connection, Action::Request), 257)
-/// ```
-pub fn packet_id_hash(family: Family, action: Action) -> EOInt {
-    ((family as EOInt) << 8) | action as EOInt
-}
-
 mod talk_reply;
 pub use talk_reply::TalkReply;
 mod init_reply;
@@ -43,17 +24,5 @@ mod account_reply;
 pub use account_reply::AccountReply;
 mod login_reply;
 pub use login_reply::LoginReply;
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    #[test]
-    fn packet_id_hash() {
-        assert_eq!(super::packet_id_hash(Family::Init, Action::Init), 65535);
-        assert_eq!(
-            super::packet_id_hash(Family::Connection, Action::Request),
-            257
-        );
-        assert_eq!(super::packet_id_hash(Family::Welcome, Action::Reply), 1283);
-    }
-}
+mod packet_processing;
+pub use packet_processing::*;
