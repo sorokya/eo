4595a4cf6784a09039055e3634dd55798ce7f8a6
Richard Leek <richard@richardleek.com>
2021-01-03 14:42:44 -0500

    refactored item file reading code

--- src/data/pubs/item/file.rs
+++ src/data/pubs/item/file.rs
@@ -12,6 +12,7 @@ use crate::data::{pubs::item::*, pubs::*, *};
 #[derive(Debug, Default)]
 pub struct ItemFile {
     pub revision: EOInt,
+    length: usize,
     pub records: Vec<ItemRecord>,
 }
 
@@ -20,6 +21,7 @@ impl ItemFile {
     pub fn new() -> Self {
         Self {
             revision: 0,
+            length: 0,
             records: Vec::default(),
         }
     }
@@ -36,39 +38,56 @@ impl ItemFile {
     ///
     pub fn read<R: Read + Seek>(&mut self, buf: &mut R) -> std::io::Result<()> {
         buf.seek(SeekFrom::Start(3))?;
-
-        let mut revision_bytes: Vec<EOByte> = vec![0; 4];
-        buf.read_exact(&mut revision_bytes)?;
-        self.revision = decode_number(revision_bytes.as_slice());
-
-        let mut length_bytes: Vec<EOByte> = vec![0; 2];
-        buf.read_exact(&mut length_bytes)?;
-        let length = decode_number(length_bytes.as_slice()) as usize;
-        self.records = Vec::with_capacity(length);
-
+        self.read_revision(buf)?;
+        self.read_length(buf)?;
         buf.seek(SeekFrom::Current(1))?;
-
-        for id in 1..length {
-            let mut size_of_name_bytes: Vec<EOByte> = vec![0; 1];
-            buf.read_exact(&mut size_of_name_bytes)?;
-
-            let size_of_name = decode_number(&size_of_name_bytes) as usize;
-            let mut name_bytes: Vec<EOByte> = vec![0; size_of_name];
-            buf.read_exact(&mut name_bytes)?;
-
-            let name = String::from_utf8(name_bytes.to_vec())
-                .expect("Failed to convert byte array to string");
-
-            if name != "eof" {
-                let mut data_buf: Vec<EOByte> = vec![0; EIF_DATA_SIZE];
-                buf.read_exact(&mut data_buf)?;
-                let mut record = ItemRecord::new(id as EOInt, &name);
-                record.deserialize(&data_buf);
-                self.records.push(record);
-            }
+        self.records = Vec::with_capacity(self.length);
+        for id in 1..self.length {
+            self.read_record(id, buf)?;
         }
 
         Ok(())
+    }
+
+    fn read_revision<R: Read + Seek>(&mut self, buf: &mut R) -> std::io::Result<()> {
+        let mut revision_bytes: Vec<EOByte> = vec![0; 4];
+        buf.read_exact(&mut revision_bytes)?;
+        self.revision = decode_number(revision_bytes.as_slice());
+        Ok(())
+    }
+
+    fn read_length<R: Read + Seek>(&mut self, buf: &mut R) -> std::io::Result<()> {
+        let mut length_bytes: Vec<EOByte> = vec![0; 2];
+        buf.read_exact(&mut length_bytes)?;
+        self.length = decode_number(length_bytes.as_slice()) as usize;
+        Ok(())
+    }
+
+    fn read_record<R: Read + Seek>(&mut self, id: usize, buf: &mut R) -> std::io::Result<()> {
+        let name = self.get_record_name(buf)?;
+        if name != "eof" {
+            let mut data_buf: Vec<EOByte> = vec![0; EIF_DATA_SIZE];
+            buf.read_exact(&mut data_buf)?;
+            let mut record = ItemRecord::new(id as EOInt, &name);
+            record.deserialize(&data_buf);
+            self.records.push(record);
+        }
+
+        Ok(())
+    }
+
+    fn get_record_name<R: Read + Seek>(&self, buf: &mut R) -> std::io::Result<String> {
+        let mut size_of_name_bytes: Vec<EOByte> = vec![0; 1];
+        buf.read_exact(&mut size_of_name_bytes)?;
+
+        let size_of_name = decode_number(&size_of_name_bytes) as usize;
+        let mut name_bytes: Vec<EOByte> = vec![0; size_of_name];
+        buf.read_exact(&mut name_bytes)?;
+
+        let name =
+            String::from_utf8(name_bytes.to_vec()).expect("Failed to convert byte array to string");
+
+        Ok(name)
     }
 }
 
