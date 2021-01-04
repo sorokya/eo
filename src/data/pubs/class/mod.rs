74c22babab815e919f66ac4adefab597daeaa51a
Richard Leek <richard@richardleek.com>
2021-01-03 19:45:35 -0500

    normalize names

--- src/data/pubs/class/mod.rs
+++ src/data/pubs/class/mod.rs
@@ -2,9 +2,7 @@ const ECF_DATA_SIZE: usize = 14;
 
 mod class_type;
 pub use class_type::ClassType;
-
-mod record;
-pub use record::*;
-
-mod file;
-pub use file::ClassFile;
+mod class_record;
+pub use class_record::ClassRecord;
+mod class_file;
+pub use class_file::ClassFile;
--- src/data/pubs/item/mod.rs
+++ src/data/pubs/item/mod.rs
@@ -8,9 +8,7 @@ mod item_sub_type;
 pub use item_sub_type::ItemSubType;
 mod item_type;
 pub use item_type::ItemType;
-
-mod record;
-pub use record::*;
-
-mod file;
-pub use file::ItemFile;
+mod item_record;
+pub use item_record::ItemRecord;
+mod item_file;
+pub use item_file::ItemFile;
