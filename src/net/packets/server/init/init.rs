c7de8d1075f9072a3aaa69f4939e81b806b41b8d
Richard Leek <richard@richardleek.com>
2021-02-20 13:13:33 -0500

    did some rust generic magic for init packets

--- src/net/packets/server/init/init.rs
+++ src/net/packets/server/init/init.rs
@@ -1,52 +1,55 @@
 use num_traits::FromPrimitive;
 
 use crate::{
-    data::{EOByte, EOChar, Serializeable, StreamBuilder, StreamReader},
+    data::{EOByte, Serializeable, StreamBuilder, StreamReader},
     net::InitReply,
 };
 
-#[derive(Debug, Default)]
+use super::{
+    InitBanned, InitFileClass, InitFileItem, InitFileMap, InitFileNPC, InitFileSpell,
+    InitFriendListPlayers, InitMapMutation, InitOk, InitOutOfDate, InitPlayers,
+};
+
 pub struct InitInit {
     pub reply_code: InitReply,
+    pub reply: Box<dyn Serializeable>,
 }
 
 impl InitInit {
     pub fn new() -> Self {
-        Self::default()
+        Self {
+            reply_code: InitReply::default(),
+            reply: Box::new(InitOk::default()),
+        }
     }
 }
 
 impl Serializeable for InitInit {
     fn deserialize(&mut self, reader: &mut StreamReader) {
-        let reply_code_char = reader.get_char();
-        self.reply_code = match InitReply::from_u8(reply_code_char) {
+        let reply_code_byte = reader.get_byte();
+        self.reply_code = match InitReply::from_u8(reply_code_byte) {
             Some(reply_code) => reply_code,
-            None => panic!("Failed to convert char to InitReply: {}", reply_code_char),
+            None => panic!("Failed to convert byte to InitReply: {}", reply_code_byte),
         };
+        self.reply = match self.reply_code {
+            InitReply::OutOfDate => Box::new(InitOutOfDate::new()),
+            InitReply::OK => Box::new(InitOk::new()),
+            InitReply::Banned => Box::new(InitBanned::new()),
+            InitReply::FileMap => Box::new(InitFileMap::new()),
+            InitReply::FileItem => Box::new(InitFileItem::new()),
+            InitReply::FileNPC => Box::new(InitFileNPC::new()),
+            InitReply::FileSpell => Box::new(InitFileSpell::new()),
+            InitReply::Players => Box::new(InitPlayers::new()),
+            InitReply::MapMutation => Box::new(InitMapMutation::new()),
+            InitReply::FriendListPlayers => Box::new(InitFriendListPlayers::new()),
+            InitReply::FileClass => Box::new(InitFileClass::new()),
+        };
+        self.reply.deserialize(reader);
     }
     fn serialize(&self) -> Vec<EOByte> {
         let mut builder = StreamBuilder::with_capacity(1);
-        builder.add_char(self.reply_code as EOChar);
+        builder.add_byte(self.reply_code as EOByte);
+        builder.append(&mut self.reply.serialize());
         builder.get()
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::{EOByte, InitInit, InitReply, Serializeable, StreamReader};
-
-    #[test]
-    fn deserialize() {
-        let buf: Vec<EOByte> = vec![0x03];
-        let mut reader = StreamReader::new(&buf);
-        let mut init = InitInit::new();
-        init.deserialize(&mut reader);
-        assert_eq!(init.reply_code, InitReply::OK);
-    }
-    #[test]
-    fn serialize() {
-        let mut init = InitInit::new();
-        init.reply_code = InitReply::OK;
-        assert_eq!(init.serialize(), [0x03]);
     }
 }
