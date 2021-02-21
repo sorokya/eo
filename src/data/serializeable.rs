312e02cd391973169ab85b4dafe503fc1ce56d70
Richard Leek <richard@richardleek.com>
2021-02-21 18:00:59 -0500

    re-arranged members in Serializable trait

--- src/data/serializeable.rs
+++ src/data/serializeable.rs
@@ -2,8 +2,8 @@ use super::{EOByte, StreamReader};
 
 /// provides types with serialization functions
 pub trait Serializeable {
-    /// serializes a type into a byte array
-    fn serialize(&self) -> Vec<EOByte>;
     /// deserializes a byte array into a type
     fn deserialize(&mut self, reader: &mut StreamReader);
+    /// serializes a type into a byte array
+    fn serialize(&self) -> Vec<EOByte>;
 }
