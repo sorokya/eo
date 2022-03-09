d44dedceeeb5d4255de4adfc8b2e7a8346544ac1
Richard Leek <richard@richardleek.com>
2022-03-09 21:49:22 +0100

    add face packets

---
+++ src/net/packets/server/face/player.rs
@@ -0,0 +1,64 @@
+use num_traits::FromPrimitive;
+
+use crate::{
+    data::{EOChar, Serializeable, StreamBuilder, StreamReader, EOShort},
+    world::Direction,
+};
+
+const PLAYER_SIZE: usize = 3;
+
+#[derive(Debug, Default)]
+pub struct Player {
+    pub player_id: EOShort,
+    pub direction: Direction,
+}
+
+impl Player {
+    pub fn new(player_id: EOShort, direction: Direction) -> Self {
+        Self {
+            player_id,
+            direction,
+        }
+    }
+}
+
+impl Serializeable for Player {
+    fn deserialize(&mut self, reader: &StreamReader) {
+        self.player_id = reader.get_short();
+        let direction_char = reader.get_char();
+        self.direction = match Direction::from_u8(direction_char) {
+            Some(direction) => direction,
+            None => panic!("Invalid direction: {}", direction_char),
+        };
+    }
+
+    fn serialize(&self) -> Vec<crate::data::EOByte> {
+        let mut builder = StreamBuilder::with_capacity(PLAYER_SIZE);
+        builder.add_short(self.player_id);
+        builder.add_char(self.direction as EOChar);
+        builder.get()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::data::EOByte;
+
+    use super::*;
+
+    #[test]
+    fn serialize() {
+        let packet = Player::new(1, Direction::Down);
+        assert_eq!(packet.serialize(), [2, 254, 1])
+    }
+
+    #[test]
+    fn deserialize() {
+        let data: Vec<EOByte> = vec![2, 254, 1];
+        let mut packet = Player::default();
+        let reader = StreamReader::new(&data);
+        packet.deserialize(&reader);
+        assert_eq!(packet.player_id, 1);
+        assert_eq!(packet.direction, Direction::Down);
+    }
+}
--- src/net/packets/server/mod.rs
+++ src/net/packets/server/mod.rs
@@ -1,6 +1,7 @@
 pub mod account;
 pub mod character;
 pub mod init;
+pub mod face;
 pub mod login;
 mod sequencer;
 pub mod welcome;
