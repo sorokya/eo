6e5b91c74c805a0d053ce23a886d8737d02fad92
Richard Leek <richard@richardleek.com>
2022-03-10 10:57:38 +0100

    remove "none" warp animation

--- src/world/warp_animation.rs
+++ src/world/warp_animation.rs
@@ -1,13 +1,12 @@
 /// describes the animation used when warping
 #[derive(Debug, Clone, Copy, PartialEq, Primitive)]
 pub enum WarpAnimation {
-    r#None = 0,
     Scroll = 1,
     Admin = 2,
 }
 
 impl Default for WarpAnimation {
     fn default() -> Self {
-        WarpAnimation::r#None
+        WarpAnimation::Scroll
     }
 }
