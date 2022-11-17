2677a39081381f2e580d544e06ddec9b01302bf5
Richard Leek <richard@richardleek.com>
2022-11-17 13:11:33 -0500

    make state copy

--- src/net/client_state.rs
+++ src/net/client_state.rs
@@ -1,5 +1,5 @@
 /// describes the state of a client
-#[derive(Debug, Eq, PartialEq, Clone)]
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
 pub enum ClientState {
     Uninitialized,
     Initialized,
